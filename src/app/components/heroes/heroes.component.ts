1f8d3bf081c0c58b98a964cd06f8eb0766b10e08
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-20 18:40:51 -0300

    add hero type

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from 'src/app/services/heroes.service';
+import { Heroe, HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
+
   constructor(private _heroesService: HeroesService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+  }
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class HeroesService {
-  private heroes: any = [
+  private heroes: Heroe[] = [
     {
       nombre: 'Aquaman',
       bio: 'El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.',
@@ -56,7 +56,15 @@ export class HeroesService {
 
   constructor() {}
 
-  getHeroes() {
+  getHeroes(): Heroe[] {
     return this.heroes;
   }
+}
+
+export interface Heroe {
+  nombre: string;
+  bio: string;
+  img: string;
+  aparicion: string;
+  casa: string;
 }
